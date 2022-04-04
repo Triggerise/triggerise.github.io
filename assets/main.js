2933084c5fcbc886e352c2373f538e4f066f4908
KevJ2021 <93124487+KevJ2021@users.noreply.github.com>
2022-04-04 12:21:32 +0200

    [OFE-399] - Fix slider on the home page (#96)
    * [OFE-399] - Fix slider on the home page
    
    * [OFE-399] - Repositioning of Our Model
    
    Co-authored-by: Kevin Jacobs <kevinjacobs-triggerise@Kevins-MacBook-Pro.local>

--- assets/main.js
+++ assets/main.js
@@ -151,6 +151,66 @@ window.onload = function () {
   });
 };
 
+var slideIndex = 0;
+showSlides();
+
+// Auto play slider on the Home page
+var autoPlaySlider = setInterval(function () {
+  showSlides(slideIndex += 1);
+}, 5000);
+
+// Toggle to next slide on the Home page
+function nextSlide() {
+  continueAutoPlay = false;
+  showSlides(slideIndex += 1);
+}
+
+// Toggle to selected slide on the Home page
+function currentSlide(n) {
+  clearInterval(autoPlaySlider);
+  showSlides(slideIndex = n);
+}
+
+// Toggle to previous slide on the Home page
+function prevSlide() {
+  clearInterval(autoPlaySlider);
+  showSlides(slideIndex -= 1);
+}
+
+// Toggle slides on the Home page
+function showSlides(n) {
+
+  var slides = document.getElementsByClassName("slidr");
+  var dots = document.getElementsByClassName('dot-slidr');
+
+  if (n >= 3) {
+    slideIndex = 0;
+  } else if (n <= -1) {
+    slideIndex = 2;
+  }
+
+  if (slides) {
+    document.querySelectorAll('.slidr').forEach(function (ele, key) {
+      if (slideIndex !== key) {
+        ele.style.display = "none";
+      } else {
+        ele.style.display = "block";
+      }
+    });
+  }
+
+  if(dots) {
+    document.querySelectorAll('.dot-slidr').forEach(function (ele, key) {
+      if (slideIndex !== key) {
+        ele.classList.remove('dot-slidr-active');
+      } else {
+        ele.classList.add('dot-slidr-active');
+      }
+    });
+  }
+
+}
+
 //carousel
 var myFullpage = new fullpage('#fullpage', {
   //Navigation
