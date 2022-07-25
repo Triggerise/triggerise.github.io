df37bdce5688b9cf4f2e49adf6ab94e3ac8a892c
KevJ2021 <93124487+KevJ2021@users.noreply.github.com>
2022-07-25 11:38:03 +0200

    Fix for menu links | Apply blank target where applicable (#112)
    Co-authored-by: Kevin Jacobs <kevinjacobs-triggerise@Kevins-MacBook-Pro.local>

--- assets/main.js
+++ assets/main.js
@@ -567,7 +567,6 @@ $('.card-flip').hover(function () {
 
 var anchors = document.querySelectorAll('a');
 anchors.forEach((a) => {
-  a.setAttribute('target', '__blank');
   a.setAttribute('rel', 'noopener noreferrer');
 });
 
